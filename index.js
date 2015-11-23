7acb4f058685a9a7f7745c6ee7403da44b8c811e
noerw <noerw@gmx.de>
2015-11-23 23:25:28 +0100

    add basic server functionality
    - add package.json
    - add express server (static file serving in -/public)
    - add mongoose database connection and a data schema stub
    - add a config file
    
    all components are saved in different files/modules

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * @desc entrypoint for the webserver. provides routes & queries to the DB
+ */
+
+var config  = require('./config.js');
+var mongo   = require('./dbConnector.js');
+var express = require('express');
+
+var app          = express();
+var publications = mongo.models.publications;
+
+/* connect to mongoDB & launch express webserver */
+mongo.connect(
+	config.dbPort,
+	config.dbName,
+	function(err) {
+		if (err) {
+			console.error('ABORTING: ' + err);
+			process.exit(1);
+		}
+
+		// once DB is connected, start webserver
+		console.log('connection to database established on port ' + config.dbPort);
+		app.listen(config.httpPort, function(){
+			console.log('http server now listening on port ' + config.httpPort);
+		});
+	}
+);
+
+/* serve everything in the folder './public/' */
+app.use(express.static(__dirname + '/public'));
