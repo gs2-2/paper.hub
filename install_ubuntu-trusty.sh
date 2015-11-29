92bc9c5e85e106346c14302528ce9427571f6290
noerw <noerw@gmx.de>
2015-11-29 17:57:18 +0100

    fix install script: LaTeXML v0.8.1, more R packages

--- install_ubuntu-trusty.sh
+++ install_ubuntu-trusty.sh
@@ -32,21 +32,21 @@ curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 # install required debian packages
 apt-get update # need to run update again, to fetch new versions from the foreign repos
 
-apt-get install -y \
 # R + R deps
-r-base-dev libgdal-dev libproj-dev \
+apt-get install -y r-base-dev libgdal-dev libproj-dev
 # LaTeXML deps
+apt-get install -y \
 libarchive-zip-perl libfile-which-perl libimage-size-perl  \
 libio-string-perl libjson-xs-perl libparse-recdescent-perl \
 liburi-perl libuuid-tiny-perl libwww-perl                  \
 libxml2 libxml-libxml-perl libxslt1.1 libxml-libxslt-perl  \
 texlive-full imagemagick libimage-magick-perl
 # all the other things
-mongodb-org nodejs
+apt-get install -y mongodb-org nodejs
 
 # download and compile LaTeXML 0.8.1
 wget http://dlmf.nist.gov/LaTeXML/releases/LaTeXML-0.8.1.tar.gz
-tar-xzf LaTeXML-0.8.1.tar.gz
+tar -xzf LaTeXML-0.8.1.tar.gz
 cd LaTeXML-0.8.1/
 perl Makefile.PL
 make
