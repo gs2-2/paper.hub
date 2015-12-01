78c196611da61a708a3c24656d09bd32c6fb3e06
LEinfeldt <lasseeinfeldt@gmail.com>
2015-12-01 12:24:16 +0100

    add DB-entry
    Write file to the DB, create DIR for the new paper.

--- index.js
+++ index.js
@@ -5,12 +5,13 @@
  */
 
 var config  = require('./config.js');
+var util = require('./util.js');
 var mongo   = require('./dbConnector.js');
 var express = require('express');
 var multer = require('multer');
-var upload = multer({dest: './public' });
-var cp = require('child_process');
-
+var bodyparser = require('body-parser');
+var cp = require('child_process')
+;
 var app          = express();
 var publications = mongo.models.publications;
 
@@ -35,18 +36,67 @@ mongo.connect(
 /* serve everything in the folder './public/' */
 app.use(express.static(__dirname + '/public'));
 
+//specify the multer upload
+/*muss umsortiert werden, wenn man die ID des papers hat und das DIR angelegt wurde. 
+Sonst sehe ich keine Möglichkeit die paper jeweils in das richtige dir einzuordnen.*/
+app.use(multer({dest: './data/papers/uploads', 
+		//rename the file to avoid name conflicts
+		rename: function(fieldname, filename) {
+			return filename;
+		},
+		//log the start of the upload process
+		onFileUploadStart: function(file) {
+			console.log(file.originalname + 'upload started');
+		},
+		//log completed upload status
+		onFileUploadComplete: function(file) {
+			console.log(file.fieldname + 'uploaded to ' + file.path);
+			done = true;
+		}
+	}));
+
 
 /* Provide express route for the LaTeX Code commited by the user*/
-app.post('/addPaper', upload.single('latexFile'), function(req, res) {
-	//read the file from the upload with multer
-	var latexFile = req.files;
-	var scriptPath = __dirname + '/bin/nicos ML script*/';
+app.post('/addPaper', upload.single('latex'), function(req, res) {
+
+	//create new paper instance in the DB
+	var uploadedPaper = new dbConnector.models.publicationModel({
+		title: req.body.title,
+		abstract: req.body.abstract,
+		author: req.body.author,
+		publicationDate: new Date(),
+		widgets: [] // koennen erst eingesetzt werden, wenn die mit den anderen Scripten erstellt wurden?!?
+	});
+
+	//save the new publication in DB
+	uploadedPaper.save(function(error) {
+		var message = error ? 'failed to save paper: ' + error 
+                            : 'paper saved: ' + uploadedPaper._id;
+        console.log(message + ' from ' + req.connection.remoteAddress);
+	});
+
+	var paperID = uploadedPaper._id;
+
+	//create a path for the new paper in the file system
+	util.newPaperDir('./data/papers/', paperID, callback); 
+	
+	//upload the file
+	upload(req, res, function(err) {
+		if(err) res.send('Error uploading the file.');
+	});
+	var latexFile = req.file;
+
 	//call the LaTeX-ML parser as a child-process
-	cp.exec(/*COMMAND script von nico*/ , function(err, stdout, stderr) {
+	cp.exec('latexml --dest=' + req.file + '.xml' + latexFile, function(err, stdout, stderr) {
+		if(err) return callback(err);
+		callback(null);
+	});
+	//xml Datei evtl. in dem HTML Ordner (zwichen-)speichern
+	cp.exec('latexmlpost --dest=irdegenwoImSYstem' + DATEINAME.xml , function(err, stdout, stderr) {
 		if(err) return callback(err);
 		callback(null);
 	});
 	//geparstes Ergebnis speichern (Dateisystem)
-	//Datebankeintrag mit Verweis auf Dateipfad --> Model fuer Dateipfad
+	res.send(paperID);
 	//Antwort an den Client mit der paperID
 })
