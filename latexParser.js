8c10f162aa3190aa6e125b893d201871b95990f9
LEinfeldt <lasseeinfeldt@gmail.com>
2015-12-06 15:12:03 +0100

    Modulized the parsing functions

---
+++ latexParser.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+*@desc Provides interface for latexMl-Script, to create html output.
+*/
+
+var cp = require('child_process');
+
+/**
+* @desc generates a html file out of an xml file
+* @param
+*/
+exports.xml2html = function (inPath, outPath, callback) {
+
+	var cmd = 'latexmlpost --dest=' + outPath + inPath;
+
+	cp.exec(cmd, function(err, stdout, stderr) {
+		if(err) return callback(err);
+		callback(null);
+	});
+};
+
+
+/**
+* @desc generate a xml file out of the .tex input file
+*/
+exports.latex2xml = function (inPath, outPath, callback) {
+
+	var cmd = 'latexml --dest=' + outPath + inPath;
+
+	cp.exec(cmd, function(err, stdout, stderr) {
+		if(err) return callback(err);
+		callback(null);
+	});
+}
