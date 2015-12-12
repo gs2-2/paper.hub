6538b5fd028a36b3188dc9f2290dd8d90814104c
Antonia <antoniaicq@web.de>
2015-12-12 15:21:34 +0100

    Resize functionality for the paper-ifame on the editorPage

---
+++ public/js/editorpage_logic.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+/**
+ * @desc fit the size of the paperFrame
+ * @param id
+ */
+
+function autoResize(id){
+    var newheight;
+    var newwidth;
+
+    if(document.getElementById){
+        newheight = document.getElementById(id).contentWindow.document .body.scrollHeight;
+        newwidth = document.getElementById(id).contentWindow.document .body.scrollWidth;
+    }
+
+    document.getElementById(id).height = (newheight) + "px";
+    document.getElementById(id).width = (newwidth) + "px";
+}
