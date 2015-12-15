35ef840f6ee3178e5aca461504bee9f2ccec58a9
noerw <noerw@gmx.de>
2015-12-15 12:29:48 +0100

    R2Graph: add function to load objects from RData

--- bin/R2json.R
+++ bin/R2json.R
@@ -9,57 +9,60 @@
 #
 # Created .csv and .json will get the same name as .Rdata
 #
-#
 # Parameters:
 # objectpath = path, where .Rdata is stored
-
-# Problem: line26, need to find a way to get the variablename of loaded .Rdata file
+# 
 # ToDo: Change RegEx for paths
 require(RJSONIO)
 
 
+#' @describe loads an RData file and returns the first object in it
+#' @param    path path to the Rdata file
+#' @return   the first object contained in the file
+loadRDataObj <- function(path) {
+  env <- new.env()
+  nm  <- load(path, envir = env, verbose = TRUE)[1]
+  env[[nm]]
+}
+
 createJSON <- function(objectpath){
-  
-  # load .Rdata file given in parameter
-  load(objectpath)
-  
-  # object <- loaded data
-  
-  
+  # load object in given path
+  object <- loadRDataObj(objectpath)
+
   # debug
   print(objectpath)
   print(object)
-  
+
   # get file name
   filename <- sub("/home/jan/Dokumente/paper.hub/delete/", "", objectpath, fixed=TRUE)
   filename <- sub(".Rdata", "", filename, fixed=TRUE)
-  
+
   # debug
   print(filename)
-  
+
   # get file path
   regex <- paste(sep="", filename, ".Rdata")
   filepath <- sub(regex, "", objectpath, fixed=TRUE)
-  
+
   # debug
   print(filepath)
-  
-  
+
+
   # create path for saving <filename>.csv
   path <- paste(sep="", filepath, filename, ".csv")
-  
+
   # save <filename>.csv
   write.csv(object, path, row.names=TRUE)
-  
+
   # load created <filename>.csv
   csv <- read.csv(path, header=TRUE, sep=",", quote="\"", dec=".", fill=TRUE)
-  
+
   # convert loaded <filename>.csv to JSON
   json <- toJSON(csv)
-  
+
   # create path for saving <filename>.json
   jsonpath <- paste(sep="",filepath, filename, ".json")
-  
+
   # save <filename>.json
   write(json, jsonpath)
 
