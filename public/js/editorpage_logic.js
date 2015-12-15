64447c0dfbb12e20c4eecba520f7daf327ae5ae8
noerw <noerw@gmx.de>
2015-12-15 13:24:48 +0100

    code cleanup
    - refactored indentation
    - made comments clearer

--- public/js/editorpage_logic.js
+++ public/js/editorpage_logic.js
@@ -1,14 +1,13 @@
-'use strict';
-
+"use strict";
 
 /**
- * @desc fit the height of the paperFrame
+ * @desc fit the height of the paperFrame to its inner dimensions
  * @param id
  */
 function autoResize(id){
-    var newHeight;
-    if(document.getElementById){
-        newHeight = document.getElementById(id).contentWindow.document .body.scrollHeight;
-    }
-    document.getElementById(id).height = (newHeight) + "px";
+	var newHeight;
+	if(document.getElementById){
+		newHeight = document.getElementById(id).contentWindow.document.body.scrollHeight;
+	}
+	document.getElementById(id).height = (newHeight) + "px";
 }
