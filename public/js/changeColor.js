deaa507c660825aed2b5fd5f0f6e419090e488f2
LEinfeldt <lasseeinfeldt@gmail.com>
2016-01-28 14:46:16 +0100

    enable changing colors in flot

--- public/js/changeColor.js
+++ public/js/changeColor.js
@@ -1,5 +1,5 @@
 /**
-* @desc Change the color of a timeseries, based on rickshaw.
+* @desc Change the color of a timeseries.
 */
 
 var colorControls = function(args) {
@@ -9,14 +9,71 @@ var colorControls = function(args) {
 
 		//save the parameters given to the function
 		this.element = args.element;
-		this.graph = args.graph;
+		this.plot = args.plot;
+		this.options = args.options;
+		this.data = args.data;
 		this.settings = this.serialize();
 
-		//define three selectable color-schemes, form rickshaw
-		var munin = new Rickshaw.Color.Palette({scheme: 'munin'});
-		var colorwheel = new Rickshaw.Color.Palette({scheme: 'colorwheel'});
-		var spectrum14 = new Rickshaw.Color.Palette({scheme: 'spectrum14'});
+		//define three selectable color-schemes, form rickshaw exmaple page
+		var munin = [
+			'#00cc00',
+			'#0066b3',
+			'#ff8000',
+			'#ffcc00',
+			'#330099',
+			'#990099',
+			'#ccff00',
+			'#ff0000',
+			'#808080',
+			'#008f00',
+			'#00487d',
+			'#b35a00',
+			'#b38f00',
+			'#6b006b',
+			'#8fb300',
+			'#b30000',
+			'#bebebe',
+			'#80ff80',
+			'#80c9ff',
+			'#ffc080',
+			'#ffe680',
+			'#aa80ff',
+			'#ee00cc',
+			'#ff8080',
+			'#666600',
+			'#ffbfff',
+			'#00ffcc',
+			'#cc6699',
+			'#999900'
+		];
 
+		var colorwheel = [
+			'#b5b6a9',
+			'#858772',
+			'#785f43',
+			'#96557e',
+			'#4682b4',
+			'#65b9ac',
+			'#73c03a',
+			'#cb513a'
+		].reverse();
+
+		var spectrum14 = [
+			'#ecb796',
+			'#dc8f70',
+			'#b2a470',
+			'#92875a',
+			'#716c49',
+			'#d2ed82',
+			'#bbe468',
+			'#a1d05d',
+			'#e7cbe6',
+			'#d8aad6',
+			'#a888c2',
+			'#9dc2d3',
+			'#649eb9',
+			'#387aa3'
+		].reverse();
 		//save the input form the form in HTML
 		this.inputs = {
 			color: this.element.elements.color
@@ -33,40 +90,25 @@ var colorControls = function(args) {
 
 			this.settings = this.serialize();
 
-			//set the config for the updated series
-			var config = {
-				series: content.series
-			};
 			//check the selected color for changing it to:
 			if(this.settings.color == 'Colorwheel') {
 
-				/* iterate over all timeseries */
-				for(var j=0; j<content.series.length; j++){
-
-					/* set color for each timeseries to colorwheel*/
-					config.series[j]["color"] = colorwheel.color();
-				};
+				//set the config for the updated series
+				this.options.colors = colorwheel;
 			}
 			else if(this.settings.color == 'Munin') {
-				/* iterate over all timeseries to munin */
-				for(var j=0; j<content.series.length; j++){
-
-					/* set color for each timeseries */
-					config.series[j]["color"] = munin.color();
-				};
+				
+				//set the config for the updated series
+				this.options.colors = munin;
 			}
 			else if(this.settings.color == 'Spectrum14') {
-				/* iterate over all timeseries to spectrum14*/
-				for(var j=0; j<content.series.length; j++){
-
-					/* set color for each timeseries */
-					config.series[j]["color"] = spectrum14.color();
-				};
+				
+				//set the config for the updated series
+				this.options.colors = spectrum14;
 			}
-			//configure the graph with the new color
-			this.graph.configure(config);
-			//and render it
-			this.graph.render();
+			this.plot = $.plot('#placeholder', this.data, this.options);
+			this.plot.setupGrid();
+			this.plot.draw();
 		}.bind(this), false);
 	};
 
